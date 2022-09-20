ae24cf773c306876475ced204ed6cb2f5b655109
Hyungmin Cho <31532996+min-cho@users.noreply.github.com>
2022-09-21 01:30:42 +0900

    Update value.js

--- models/value.js
+++ models/value.js
@@ -4,11 +4,11 @@ var ValueSchema = mongoose.Schema({
   detectionID: {type: String, required: true},
   var_names: [],
   var_data: [{
-    v1:{type:Date}, 
+    time:{type:String}, 
+    v1:{type:Number}, 
     v2:{type:Number}, 
     v3:{type:Number}, 
-    v4:{type:Number}, 
-    v5:{type:Number}
+    v4:{type:Number}
   }]
   });
 
